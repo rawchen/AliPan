926dff58891b4522d126921eee875aa3943ea785
RawChen <2221999792@qq.com>
2021-11-08 21:43:34 +0800

    feat: 后端路由

--- src/main/java/com/rawchen/alipan/controller/IndexController.java
+++ src/main/java/com/rawchen/alipan/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.rawchen.alipan.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,8 +17,23 @@ public class IndexController {
 		System.out.println("Index启动");
 //		return "redirect:"+"/folder2/root";
 //		return "forward:"+"/down/" + ;
+		model.addAttribute("type", "folder");
 		model.addAttribute("fileId", parentFileId);
 		return "index2";
 
+	}
+
+	@RequestMapping("/folder/{fileId}")
+	public String toFolder(@PathVariable("fileId") String fileId, Model model){
+		model.addAttribute("fileId", fileId);
+		model.addAttribute("type", "folder");
+		return "index2";
+	}
+
+	@RequestMapping("/file/{fileId}")
+	public String toFile(@PathVariable("fileId") String fileId, Model model){
+		model.addAttribute("fileId", fileId);
+		model.addAttribute("type", "file");
+		return "index2";
 	}
 }
